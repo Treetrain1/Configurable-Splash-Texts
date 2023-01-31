8f0d14eb4492230882c25acb033670f6a679452e
Treetrain1 <treetrain1@gmail.com>
2023-01-31 01:10:54 -0600

    Remove Fabric API and Mod Menu dependencies

--- src/main/java/com/treetrain1/configurablesplashtexts/config/defaultconfig/DefaultGeneralConfig.java
+++ src/main/java/com/treetrain1/configurablesplashtexts/config/defaultconfig/DefaultGeneralConfig.java
@@ -11,6 +11,7 @@ public class DefaultGeneralConfig {
 	public static final List<String> ADDED_SPLASHES = new ArrayList<>();
 
 	public static final List<String> REMOVED_SPLASHES = new ArrayList<>();
+
 	public static final int SPLASH_COLOR = DyeColor.YELLOW.getTextColor();
 
 	public static final boolean REMOVE_VANILLA = false;
